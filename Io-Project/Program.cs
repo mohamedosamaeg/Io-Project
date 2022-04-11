3467a273d49060c36fa8ccf7f6cb251461ce868f
Mohamed Osama <73117933+mohamedosamaeg@users.noreply.github.com>
2022-04-12 00:00:27 +0200

    Add project files.

---
+++ Io-Project/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome To IO System :)");
+
+for (; ; )
+{
+    Console.WriteLine("what do you want:)?\n"+
+        "1.Manage Folders\n" +
+        "2.close cmd\n");
+
+    int MainAction = Convert.ToInt32(Console.ReadLine());
+    if (MainAction == 1)
+
+    {
+        for (; ; )
+        {
+            Console.WriteLine("choose action\n" +
+                "1.Report Folder!\n"+
+                "2.close cmd\n");
+            int Action = Convert.ToInt32(Console.ReadLine());
+            switch (Action)
+            {
+                case 1:
+                    try
+                    {
+                        Console.WriteLine("Insert folder path Plz");
+                        string FolderPath = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(FolderPath))
+                        {
+                            if (Directory.Exists(FolderPath))
+                            {
+                                List<FolderReportDto> folderReportDtos = new List<FolderReportDto>();
+                                foreach (string FilePath in Directory.GetFiles(FolderPath))
+                                {
+                                    DateTime LastAccessDate = File.GetLastAccessTime(FilePath);
+                                    Console.WriteLine("Enter Date Plz");
+                                    DateTime lastAccessDate = Convert.ToDateTime(Console.ReadLine());
+
+                                    string FileExtention = Path.GetExtension(FilePath);
+                                    if ((FilePath > File.LastAccessDate);
+                                    {
+                                        FolderReportDto.Where(FilePath > LastAccessDate);
+                                    }
+                                    else
+                                    {
+                                        FolderReportDto.Where(FilePath < LastAccessDate);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong path:( plz Try Again! ");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please insert Your folder path");
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Beep();
+                        Console.WriteLine($"Exeption: {ex.Message}\nDate of error: {DateTime.Now}\n\n");
+                    }
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+    else if (MainAction == 2)
+    {
+        break;
+    }
+}
+public class FolderReportDto
+{
+    public string ExtentionType { get; set; }
+    public int ExtentionCount { get; set; }
+    public List<FolderReportDto> Folders { get; set; }
+}
